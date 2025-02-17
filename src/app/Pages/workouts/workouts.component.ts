efcdaaada07b35262fb8fa7e5198ac4334f4726b
Heinrich <heinrichlam2@gmail.com>
2025-02-17 12:55:10 +0200

    Ephesians Fitness
    Updated the pdf docs for the Workouts, These are simple workouts made with ChatGPT.
    They will all get updated once the programs get finalized.

--- src/app/Pages/workouts/workouts.component.ts
+++ src/app/Pages/workouts/workouts.component.ts
@@ -125,17 +125,17 @@ serverHost: string = 'https://ephesians-fitness-server.onrender.com';
 
   getWorkoutFileUrl(programId: number): string{
     switch(programId){
-      case 1: return 'assets/training/Full-Body-Strength.pdf';
-      case 2: return 'assets/training/PPL-Gym-Split.pdf';
-      case 3: return 'assets/training/Upper-Lower-Split.pdf';
-      case 4: return 'assets/training/Powerlifting.pdf';
-      case 5: return 'assets/training/Hypertrophy-Circuit.pdf';
-      case 6: return 'assets/training/Bodyweight-Strength.pdf';
-      case 7: return 'assets/training/Calisthenics.pdf';
-      case 8: return 'assets/training/Full-Body-Bodyweight.pdf';
-      case 9: return 'assets/training/HIIT-Sprint-Cardio.pdf';
-      case 10: return 'assets/training/Cardio-Endurance.pdf';
-      default: return 'assets/training/PPL-Gym-Split.pdf';
+      case 1: return '/training/Full-Body-Strength.pdf';
+      case 2: return '/training/PPL-Gym-Split.pdf';
+      case 3: return '/training/Upper-Lower-Split.pdf';
+      case 4: return '/training/Powerlifting.pdf';
+      case 5: return '/training/Hypertrophy-Circuit.pdf';
+      case 6: return '/training/Bodyweight-Strength.pdf';
+      case 7: return '/training/Calisthenics.pdf';
+      case 8: return '/training/Full-Body-Bodyweight.pdf';
+      case 9: return '/training/HIIT-Sprint-Cardio.pdf';
+      case 10: return '/training/Cardio-Endurance.pdf';
+      default: return '/training/PPL-Gym-Split.pdf';
     }
   }
 
